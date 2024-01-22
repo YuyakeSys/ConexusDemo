7a6583e617b49c6530cb9d6c6a6b04594b65e5fb
CheserMeng <75210411+YuyakeSys@users.noreply.github.com>
2024-01-22 15:24:28 +0100

    Initial set up
    Devise_api set up
    API/V1 for routes
    Basic set up for devise users and posts

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # API routes should be in /api/v1
+  namespace :api do
+    namespace :v1 do 
+      resources :posts
+    end 
+  end 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
